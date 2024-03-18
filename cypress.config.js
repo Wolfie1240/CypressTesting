d49ba86ff9f8305ec86ff92c7c2d1907faecc54e
wolfle <lwolf1240@gmail.com>
2024-03-17 17:14:02 -0700

    fixing more stuff

--- cypress.config.js
+++ cypress.config.js
@@ -4,11 +4,10 @@ module.exports = defineConfig({
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
-    setupNodeEvents(on, config) {
-      on('file:preprocessor', (file) => {
-        // ...
-      })
+    setupNodeEvents(on, config){
+
     },
+    baseUrl: "http://localhost:8080"
   },
 })
 
